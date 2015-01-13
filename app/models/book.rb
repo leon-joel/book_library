feec340b77d9dd18b9f3172db1f1ba8b55d06090
L.Joel <nakaue+gh@gmail.com>
2015-01-13 19:06:11 +0900

    AtmarkIT Coding Edge , Rails Hub, Rails4 Nyuumon 6 Finished.

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,10 @@
 class Book < ActiveRecord::Base
+  # refer user model      * http://www.atmarkit.co.jp/ait/articles/1406/30/news030.html
+  belongs_to :user
+
+  has_many :taggings
+  has_many :tags, through: :taggings
+
+
+  scope    :tagged_recommended, -> { joins(:tags).where(tags: { name: 'recommended' } )}
 end
